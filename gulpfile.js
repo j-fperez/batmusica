2e30b087a4f170bc4523010dd474cb1a4daa2fc0
Jose Francisco Perez <josefcoperez@yahoo.es>
2016-10-16 03:38:01 +0200

    Hemos creado partials y estamos empezando a crear las secciones de las canciones

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,33 @@
 // importamos gulp
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var notify = require('gulp-notify');
+var browserSync = require ('browser-sync').create();
 
 // definimos tarea por defecto
 gulp.task("default", function() {
-	console.log("Hello World");
+
+	// iniciar browserSync
+	browserSync.init({
+		server: "./", //levanta el servidor en la carpeta actual
+		browser: "google chrome"
+	});
+
+	//observa cambios en archivos SASS y ejecuta la tarea de compilación
+	gulp.watch("src/scss/*.scss", ["compile-sass"]);
+
+	// observa cambios en archivos HTML y recargue el navegador
+	gulp.watch("*.html").on("change", browserSync.reload); 
 });
 
 // definimos la tarea para compilar SAAS
 gulp.task("compile-sass", function(){
 	gulp.src("./src/scss/style.scss") // cargamos el archivo
-	.pipe(sass()) // compilamos el archivo SASS
+	.pipe(sass().on('error', sass.logError)) // compilamos el archivo SASS y controlamos los errores
 	.pipe(gulp.dest("./dist/css/")) // guardamos el archivo en dist/css
+	.pipe(notify({
+		title: "SASS",
+		 message: "Compiled!"
+	}))
+	.pipe(browserSync.stream());
 });
